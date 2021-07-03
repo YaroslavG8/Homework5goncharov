bd603be3eb80d134b1214618940ac321d14166a0
Yaroslav <setevoeo@gmail.com>
2021-07-03 11:29:56 +0300

    Initial commit

---
+++ src/main/java/HomeworkTask5e.java
@@ -0,0 +1,12 @@
+public class HomeworkTask5e {
+
+    public static void hackSalary(int salary){
+        int s = salary + 100;
+        System.out.println("Твоя зарплата составляет: " + s + " долларов в месяц.");
+    }
+
+    public static void main(String[] args) {
+        HomeworkTask5e.hackSalary(900);
+
+    }
+}
